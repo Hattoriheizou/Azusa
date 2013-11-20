650c3fadf928cf71e3e6688b3ed3d59b13a6dfaf
uchuutamashi <stevenytc@gmail.com>
2013-11-20 18:34:26 -0500

    +RID Link  + Ports

--- IOServer proto/IOServer_proto/IOServer_proto/IOPortedPrc.cs
+++ IOServer proto/IOServer_proto/IOServer_proto/IOPortedPrc.cs
@@ -6,15 +6,31 @@ using System.Diagnostics;
 
 namespace IOServer_proto
 {
+    enum EngineType{Input,Output,AI,Routine }
+
+    struct RIDLinkage{
+        public string RID;
+        public bool ArgOnly;
+    }
+
+
     class IOPortedPrc
     {
         public string Name;
         public Process Engine;
+        public EngineType Type;
+
+        public List<int> Ports = new List<int>();
+        public List<RIDLinkage> RIDs = new List<RIDLinkage>();
 
         public IOPortedPrc(string name, string enginePath, string arg = "")
         {
 
             Name = name;
+            
+            //default type is Routine until the Engine self-identifies itself
+            Type = EngineType.Routine;
+
             //specifies the way the recognizer is run
             Engine = new Process();
             Engine.StartInfo = new ProcessStartInfo(enginePath, arg);
@@ -43,6 +59,8 @@ namespace IOServer_proto
 
             Engine.Start();
             Engine.BeginOutputReadLine();
+            Engine.StandardInput.AutoFlush = true;
+
             dummyAZUSA.Print("[" + Name + " has started.]");
 
         }
@@ -78,6 +96,9 @@ namespace IOServer_proto
         void Engine_Exited(object sender, EventArgs e)
         {
             ProcessManager.RemoveProcess(this);
+            Engine.CancelOutputRead();
+
+
             dummyAZUSA.Print("[" + Name + " has ended.]");
 
             foreach (IOPortedPrc prc in ProcessManager.GetCurrentProcesses())
@@ -88,9 +109,17 @@ namespace IOServer_proto
 
         void Engine_OutputDataReceived(object sender, DataReceivedEventArgs e)
         {
-            //from who??
-            dummyAZUSA.Print(sender.ToString() + " : " + e.Data);
-            
+            dummyAZUSA.Print(Engine.Id + " : " + e.Data);
+
+            if (e.Data.Trim() == "NYAN")
+            {
+                Engine.StandardInput.WriteLine("NYAN");
+            }
+
+            if (e.Data.Trim() == "WAIT")
+            {
+                System.Threading.Thread.Sleep(8000);
+            }
         }
 
 
