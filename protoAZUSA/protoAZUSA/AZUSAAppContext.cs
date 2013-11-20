78125c7744ae496e8c9e7127b3046a2793413532
uchuutamashi <stevenytc@gmail.com>
2013-11-20 18:39:37 -0500

    initial commit

---
+++ protoAZUSA/protoAZUSA/AZUSAAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text;
+
+namespace AZUSA
+{
+    public class AZUSAAppContext : ApplicationContext
+    {
+        NotifyIcon notifyIcon = new NotifyIcon();
+
+        public AZUSAAppContext()
+        {           
+
+            notifyIcon.Icon = AZUSA.Properties.Resources.icon;            
+            notifyIcon.Visible = true;
+        }
+    }
+}
